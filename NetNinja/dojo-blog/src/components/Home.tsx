c6050fd7b8112962febbb844f66f53790aa366f6
orvinjedalava <ojedbub@gmail.com>
2024-08-28 16:44:55 +1000

    Updates from finishing Tutorial #8 - State (useState hook)

--- NetNinja/dojo-blog/src/components/Home.tsx
+++ NetNinja/dojo-blog/src/components/Home.tsx
@@ -1,18 +1,19 @@
+import { useState } from 'react';
+
 export const Home: React.FC = () => {
+    const [name, setName] = useState('jed');
+    const [age, setAge] = useState(25);
 
     const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-        console.log('Hello, ninjas', e);
-    }
-
-    const handleClickAgain = (name: string, e: React.MouseEvent<HTMLButtonElement>) => {
-        console.log('hello ' + name, e.target);
+        setName('mel');
+        setAge(20);
     }
 
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ name } is { age } years old</p>
             <button onClick={handleClick}>Click Me</button>
-            <button onClick={(e) => handleClickAgain('Jed', e)}>Click me again</button>
         </div>
     )
 }
