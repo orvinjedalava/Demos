3324e8b5697c8d555a6a0d0f3da1de3373eb28ea
orvinjedalava <ojedbub@gmail.com>
2024-08-28 16:15:26 +1000

    Update with final code after Tutorial #7 - Click Events

--- NetNinja/dojo-blog/src/components/Home.tsx
+++ NetNinja/dojo-blog/src/components/Home.tsx
@@ -1,7 +1,18 @@
 export const Home: React.FC = () => {
+
+    const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+        console.log('Hello, ninjas', e);
+    }
+
+    const handleClickAgain = (name: string, e: React.MouseEvent<HTMLButtonElement>) => {
+        console.log('hello ' + name, e.target);
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={(e) => handleClickAgain('Jed', e)}>Click me again</button>
         </div>
     )
 }
--- NetNinja/dojo-blog/src/components/Navbar.tsx
+++ NetNinja/dojo-blog/src/components/Navbar.tsx
@@ -1,12 +1,12 @@
-const navBarLinkButtonStyle = {
-    color: "white",
-    backgroundColor: '#f1356d',
-    borderRadius: '8px'
-};
-
 // React.FC is short for React.FunctionComponent. It is a type that describes a function component. 
 // It is a generic type that takes props as a type argument.
 export const Navbar: React.FC = () => {
+    const navBarLinkButtonStyle = {
+        color: "white",
+        backgroundColor: '#f1356d',
+        borderRadius: '8px'
+    };
+
     return (
         <nav className="navbar">
             <h1>The Dojo Blog</h1>
