df0810fd6aa4eb70b5f84c223f85a3e7f1d61359
orvinjedalava <ojedbub@gmail.com>
2024-08-28 17:13:44 +1000

    Update from finishing Tutorial #10 - Outputting Lists

--- NetNinja/dojo-blog/src/components/Home.tsx
+++ NetNinja/dojo-blog/src/components/Home.tsx
@@ -1,19 +1,19 @@
 import { useState } from 'react';
 
 export const Home: React.FC = () => {
-    const [name, setName] = useState('jed');
-    const [age, setAge] = useState(25);
-
-    const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-        setName('mel');
-        setAge(20);
-    }
-
+    const[blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'jed', id: 3 }
+    ]);
     return (
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{ name } is { age } years old</p>
-            <button onClick={handleClick}>Click Me</button>
+            {blogs.map(blog => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author }</p>
+                </div>
+            ))}
         </div>
     )
 }
