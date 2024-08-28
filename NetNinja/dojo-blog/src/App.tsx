de46215fcd952b45c770a13bcb466905636898d6
orvinjedalava <ojedbub@gmail.com>
2024-08-28 15:03:30 +1000

    Updates after finishing Tutorial #4 - Dynamic Values in Templates

--- NetNinja/dojo-blog/src/App.tsx
+++ NetNinja/dojo-blog/src/App.tsx
@@ -2,23 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(): JSX.Element {
+  const title: string = 'Welcome to the new blog';
+  const likes: number = 50;
+  //const person = { name: 'yoshi', age: 30 };
+  const link = "http://www.google.com";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{ title }</h1>
+        <p>Liked { likes } times</p>
+
+        <p>{ 10 }</p>
+        <p>{ "hello, ninjas" }</p>
+        <p>{ [1,2,3,4,5] }</p>
+        <p>{ Math.random() * 10}</p>
+
+        <a href={ link } >Google Site</a>
+
+      </div>
     </div>
   );
 }
