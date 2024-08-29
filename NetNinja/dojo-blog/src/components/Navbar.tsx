116f73bc10cd36860e952f0f3b18489f7a7ff212
orvinjedalava <ojedbub@gmail.com>
2024-08-29 14:23:23 +1000

    Updates from finishing Tutorial #23 - Router Links

--- NetNinja/dojo-blog/src/components/Navbar.tsx
+++ NetNinja/dojo-blog/src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 // React.FC is short for React.FunctionComponent. It is a type that describes a function component. 
 // It is a generic type that takes props as a type argument.
 export const Navbar: React.FC = () => {
@@ -11,8 +13,8 @@ export const Navbar: React.FC = () => {
         <nav className="navbar">
             <h1>The Dojo Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create" style={ navBarLinkButtonStyle }>New Blog</a>
+                <Link to="/">Home</Link>
+                <Link to="/create" style={ navBarLinkButtonStyle }>New Blog</Link>
             </div>
         </nav>
     );
