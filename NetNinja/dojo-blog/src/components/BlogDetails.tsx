61db68605d9105d93d74ed7112d7f00fb33f9415
orvinjedalava <ojedbub@gmail.com>
2024-08-29 16:54:16 +1000

    Updates from finishing Tutorial #31 - Deleting Blogs

--- NetNinja/dojo-blog/src/components/BlogDetails.tsx
+++ NetNinja/dojo-blog/src/components/BlogDetails.tsx
@@ -1,21 +1,36 @@
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import { useFetch } from "../hooks/useFetch";
 import { Data } from "../types/MyTypes";
+import { useState } from "react";
 
 export const BlogDetails = () => {
     const { id } = useParams<{ id: string }>();
     const { data, isPending, error } = useFetch('http://localhost:8000/blogs/' + id);
     const blog = data as Data;
+    const history = useHistory();
+    const [ isDeletePending, setIsDeletePending ] = useState<boolean>(false);
+
+    const handleClick = () => {
+        setIsDeletePending(true);
+        fetch('http://localhost:8000/blogs/' + blog.id, {
+            method: 'DELETE'
+        }).then(() => {
+            history.push('/');
+            setIsDeletePending(false);
+        });
+    }
 
     return (
         <div className="blog-details">
             { isPending && <div>Loading...</div> }
             { error && <div>{ error }</div>}
-            { data && (
+            { !isPending && data && (
                 <article>
                     <h2>{ blog.title }</h2>
                     <p>Written by { blog.author}</p>
                     <div>{ blog.body }</div>
+                    { !isDeletePending && <button onClick={handleClick}>Delete</button> }
+                    { isDeletePending && <button>Deleting...</button> }
                 </article>
             )}
         </div>
