7986c7fb52e9329cf70c3c5bfc07f0d7e180eb7f
orvinjedalava <ojedbub@gmail.com>
2024-08-29 10:46:21 +1000

    Updates for finishing Tutorial #14 - useEffect Hook ( the basics )

--- NetNinja/dojo-blog/src/components/Home.tsx
+++ NetNinja/dojo-blog/src/components/Home.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { BlogList } from './BlogList';
 
 export const Home: React.FC = () => {
@@ -12,6 +12,12 @@ export const Home: React.FC = () => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
+
+    // This is the function that is going to run everytime there is a re-render
+    useEffect(() => {
+        console.log('use effect ran');
+        console.log(blogs);
+    });
 
     return (
         <div className="home">
