e5b19c859a03b55a77b39d68da866ef09c166cbb
orvinjedalava <ojedbub@gmail.com>
2024-08-29 16:17:55 +1000

    Updates for finishing Tutorial #27 - Controlled Inputs (forms)

--- NetNinja/dojo-blog/src/components/Create.tsx
+++ NetNinja/dojo-blog/src/components/Create.tsx
@@ -1,7 +1,41 @@
+import { useState } from "react"
+
 export const Create = () => {
+    const [title, setTitle] = useState<string>('');
+    const [body, setBody] = useState<string>('');
+    const [author, setAuthor] = useState<string>('mario');
+
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form>
+                <label>Blog title:</label>
+                <input 
+                    type="text"
+                    required
+                    value={ title }
+                    onChange={(e:React.ChangeEvent<HTMLInputElement>) => setTitle(e.target.value)} 
+                />
+                <label>Blog body:</label>
+                <textarea 
+                    required
+                    value={ body }
+                    onChange={(e:React.ChangeEvent<HTMLTextAreaElement>) => setBody(e.target.value)}
+                >
+                </textarea> 
+                <label>Blog author:</label>
+                <select
+                    value={ author }
+                    onChange={(e:React.ChangeEvent<HTMLSelectElement>) => setAuthor(e.target.value)}
+                >
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+                <button>Add Blog</button>
+                <p>{ title }</p>
+                <p>{ body }</p>
+                <p>{ author }</p>
+            </form>
         </div>
     )
 }
