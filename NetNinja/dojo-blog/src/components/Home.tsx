e4c3df5a563e765d0ed0042f1dfa38f3bcf0f434
orvinjedalava <ojedbub@gmail.com>
2024-08-29 10:08:52 +1000

    Updates from finishing Tutorial #11 - Props

--- NetNinja/dojo-blog/src/components/Home.tsx
+++ NetNinja/dojo-blog/src/components/Home.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { BlogList } from './BlogList';
 
 export const Home: React.FC = () => {
     const[blogs, setBlogs] = useState([
@@ -8,12 +9,7 @@ export const Home: React.FC = () => {
     ]);
     return (
         <div className="home">
-            {blogs.map(blog => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>Written by { blog.author }</p>
-                </div>
-            ))}
+            <BlogList title='All Blogs' blogs={blogs}/>
         </div>
     )
 }
