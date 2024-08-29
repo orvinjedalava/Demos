43f30ffc9a02388a87c87ed4aa598041333afb33
orvinjedalava <ojedbub@gmail.com>
2024-08-29 11:52:58 +1000

    Updates from finishing Tutorial #20 - Making a Custom Hook

--- NetNinja/dojo-blog/src/components/Home.tsx
+++ NetNinja/dojo-blog/src/components/Home.tsx
@@ -11,24 +11,34 @@ type Blog = {
 export const Home: React.FC = () => {
     const[blogs, setBlogs] = useState<Blog[]>([]);
     const[isPending, setIsPending] = useState<boolean>(true);
+    const[error, setError] = useState<string | null>(null);
 
     // This is the function that is going to run everytime there is a re-render
     // We can pass a dependency array to indicate the objects that it keeps track of before running the useEffect function.
     useEffect(() => {
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                setBlogs(data);
-                setIsPending(false);
-            })
+                .then((res: Response) => {
+                    if (!res.ok) {
+                        throw Error('Could not fetch the data for that resource');
+                    }
+                    return res.json();
+                })
+                .then((data: Blog[]) => {
+                    setBlogs(data);
+                    setIsPending(false);
+                    setError(null);
+                })
+                .catch((err: Error) => {
+                    setError(err.message);
+                    setIsPending(false);
+                })
         }, 1000)
     }, []);
 
     return (
         <div className="home">
+            { error && <div>{ error }</div> }
             { isPending && <div>Loading...</div> }
             {blogs && blogs.length > 0 && <BlogList title='All Blogs' blogs={blogs}/>}
         </div>
