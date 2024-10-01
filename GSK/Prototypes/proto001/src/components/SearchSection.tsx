43f35d590b8ffd5e0c791bd8266a600bcb987481
orvinjedalava <ojedbub@gmail.com>
2024-10-01 16:54:25 +1000

    Adding a search bar

--- GSK/Prototypes/proto001/src/components/SearchSection.tsx
+++ GSK/Prototypes/proto001/src/components/SearchSection.tsx
@@ -2,6 +2,19 @@ import styles from './searchsection.module.css';
 
 export default function SearchSection() {
     return (
-        <div className={styles['background-image']}></div>
+        <div className={styles['background-image']}>
+            <form className={styles['search-form']}>
+                <div className={styles['search-form-bar']}>
+                    <i className="fa-solid fa-magnifying-glass"></i>
+                    <input type="text" placeholder="Search for products, brands, etc." />
+                    <button className={styles['btn-filter']}>
+                        {/* <i className="fa-thin fa-bars-filter"></i> */}
+                        <i className="fa-solid fa-filter"></i>
+                        <span>Filters</span>
+                    </button>
+                    <button className={styles['btn-search']} type="submit">Search</button>
+                </div>
+            </form>
+        </div>
     );
 }
