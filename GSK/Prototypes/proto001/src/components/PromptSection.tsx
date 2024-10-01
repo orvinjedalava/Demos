87f96592393c048329ff4eadc24e85fea2371a88
orvinjedalava <ojedbub@gmail.com>
2024-10-01 17:23:48 +1000

    Renaming classes

--- GSK/Prototypes/proto001/src/components/PromptSection.tsx
+++ GSK/Prototypes/proto001/src/components/PromptSection.tsx
@@ -3,8 +3,8 @@ import styles from './promptsection.module.css';
 export default function PromptSection() {
     return (
         <div className={styles['background-image']}>
-            <form className={styles['search-form']}>
-                <div className={styles['search-form-bar']}>
+            <form className={styles['prompt-form']}>
+                <div className={styles['prompt-form-bar']}>
                     <i className="fa-solid fa-magnifying-glass"></i>
                     <input type="text" placeholder="Compose a prompt here and click Generate" />
                     <button className={styles['btn-filter']}>
