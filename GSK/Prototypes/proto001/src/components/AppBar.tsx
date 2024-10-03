80a1cb169a3c17c51778ba90ac1b54c816edfe71
orvinjedalava <ojedbub@gmail.com>
2024-10-03 16:51:13 +1000

    Additional changes

--- GSK/Prototypes/proto001/src/components/AppBar.tsx
+++ GSK/Prototypes/proto001/src/components/AppBar.tsx
@@ -8,12 +8,14 @@ export default function AppBar() {
                 <img src={gskLogo} alt="GSK Logo"/>
             </div>
             <div className={styles['appbar-items']}>
-                <a href="#"><span>Company</span><i className="fa-solid fa-chevron-down"></i></a>
+                {/* <a href="#"><span>Company</span><i className="fa-solid fa-chevron-down"></i></a>
                 <a href="#"><span>Innovation</span><i className="fa-solid fa-chevron-down"></i></a>
                 <a href="#"><span>Products</span><i className="fa-solid fa-chevron-down"></i></a>
                 <a href="#"><span>Responsibility</span><i className="fa-solid fa-chevron-down"></i></a>
                 <a href="#"><span>Media</span><i className="fa-solid fa-chevron-down"></i></a>
-                <a href="#"><span>Careers</span><i className="fa-solid fa-chevron-down"></i></a>
+                <a href="#"><span>Careers</span><i className="fa-solid fa-chevron-down"></i></a> */}
+                <a href="#"><span>About</span><i className="fa-solid fa-chevron-down"></i></a>
+                <a href="#"><span>Help</span><i className="fa-solid fa-chevron-down"></i></a>
             </div>
          </div>
     );
--- GSK/Prototypes/proto001/src/components/PromptFilterDialog.tsx
+++ GSK/Prototypes/proto001/src/components/PromptFilterDialog.tsx
@@ -6,43 +6,43 @@ interface PromptFilterDialogProps {
 
 export default function PromptFilterDialog({handlePromptModalDialogClose}: PromptFilterDialogProps) {
     return (
-        <div>
+        <div className={styles['dialog-container']}>
             <form>
                 <div className={styles['header-container']}>
                     <span>Filters</span>
                     <button className={styles['close-button']} onClick={handlePromptModalDialogClose}>&times;</button>
                 </div>
                 <hr/>
-                <div className="label-container">
-                    <label>Disease types</label>
+                <div className={styles['section-title-container']}>
+                    <span>Conditions</span>
                 </div>
                 <div className="form-container">
                     <div className="column">
                         <label>
                             <input type="checkbox" name="option1" value="1" />
-                            <span className="checkmark"></span> Option 1
+                            <span className="checkmark"></span> Heart disease
                         </label>
                         <label>
                             <input type="checkbox" name="option2" value="2" />
-                            <span className="checkmark"></span> Option 2
+                            <span className="checkmark"></span> High blood pressure
                         </label>
                         <label>
                             <input type="checkbox" name="option3" value="3" />
-                            <span className="checkmark"></span> Option 3
+                            <span className="checkmark"></span> Respiratory disease
                         </label>
                     </div>
                     <div className="column">
                         <label>
                             <input type="checkbox" name="option4" value="4" />
-                            <span className="checkmark"></span> Option 4
+                            <span className="checkmark"></span> Diabetes
                         </label>
                         <label>
                             <input type="checkbox" name="option5" value="5" />
-                            <span className="checkmark"></span> Option 5
+                            <span className="checkmark"></span> Sensory impairment
                         </label>
                         <label>
                             <input type="checkbox" name="option6" value="6" />
-                            <span className="checkmark"></span> Option 6
+                            <span className="checkmark"></span> Respiratory disease
                         </label>
                     </div>
                 </div>
